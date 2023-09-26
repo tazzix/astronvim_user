0811cbe3443896bb84b09fce38a3fa24290e1d24
tazzix <phonedev@tazzix.com>
2023-09-26 19:56:56 +0500

    added plugins

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,6 +4,9 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.pack.dart" },
+
+  
   -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,4 +1,33 @@
 return {
+  "kevinhwang91/nvim-bqf",
+  
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      filtered_items = {
+        visible = false,
+        show_hidden_count = true,
+        hide_dotfiles = true,
+        hide_gitignored = false,
+        hide_by_name = {
+          '.git',
+          '.DS_Store',
+          'thumbs.db',
+        },
+        never_show = {},
+      },
+    }
+  },
+
+  "folke/todo-comments.nvim",
+  lazy = false,
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+ },
+
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
   -- "andweeb/presence.nvim",
@@ -10,3 +39,13 @@ return {
   --   end,
   -- },
 }
+
+-- {
+--   'akinsho/flutter-tools.nvim',
+--   lazy = false,
+--   dependencies = {
+--       'nvim-lua/plenary.nvim',
+--       'stevearc/dressing.nvim', -- optional for vim.ui.select
+--   },
+--   config = true,
+-- }
